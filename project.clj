9d6f887a518a6767112f50beb21af4ad1ba19291
Jack <jackratner@gmail.com>
2021-03-26 23:30:07 -0400

    feat: add redis
    to enable login id storage (for magic links) and session storage (for
    request auth)

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
   :dependencies [[compojure "1.6.1"]
                  [environ "1.2.0"]
                  [hiccup "1.0.5"]
-                 [org.clojure/clojure "1.10.0"]
                  [com.github.seancorfield/next.jdbc "1.1.646"]
+                 [com.taoensso/carmine "3.1.0"]
+                 [org.clojure/clojure "1.10.0"]
                  [org.postgresql/postgresql "42.2.19.jre7"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ring/ring-defaults "0.3.2"]
@@ -17,4 +18,5 @@
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]
-         :env {:db-password "local"}}})
+         :env {:db-password "local"
+               :redis-conn "redis://localhost:6379/0"}}})
