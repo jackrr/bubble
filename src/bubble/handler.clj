fee1c0226c6eac59703e07b15c23990d0428f259
Jack <jackratner@gmail.com>
2021-04-25 11:54:13 -0400

    feat: basic login form and handler
    experimenting with grouping all mvc logic in the same top
    namespace (bubble.login) to hopefully make it easier to follow how
    different parts of the app work.

--- src/bubble/handler.clj
+++ src/bubble/handler.clj
@@ -1,6 +1,7 @@
 (ns bubble.handler
   (:require [bubble.db :as db]
             [bubble.views :as views]
+            [bubble.login :as login]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.logger :as logger]
@@ -10,7 +11,7 @@
   (let [{:keys [params uri]} req
         param-name (get params :bubblename)
         address (get params :participantaddress)
-        req-type (if (= uri "/get-submit") "GET" "POST")] 
+        req-type (if (= uri "/get-submit") "GET" "POST")]
     (println params)
     (db/make-bubble param-name)
     (str
@@ -21,8 +22,11 @@
       </div>")))
 
 (defroutes app-routes
+  ;; TODO: redirect to login if not logged in
   (GET "/" [] (views/index-page (db/bubble-count)))
   (POST "/newbubble" req (display-result req))
+  (GET "/login" [] (login/form-page))
+  (POST "/login" req (login/handle-request req))
   (route/not-found "Not Found"))
 
 (def app
