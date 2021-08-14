d7910ff31009b203f6b24f0657e557fa6682a5a5
Jack <jackratner@gmail.com>
2021-08-14 11:07:55 -0400

    feat: views/style fn
    takes in key/value pairs, spits out a hiccup-compliant hashmap of style
    rules

--- src/bubble/views.clj
+++ src/bubble/views.clj
@@ -1,6 +1,11 @@
 (ns bubble.views
   (:use [hiccup core page]))
 
+(defn style [& info]
+  {:style (.trim (apply str (map #(let [[kwd val] %]
+                                    (str (name kwd) ":" val "; "))
+                                 (apply hash-map info))))})
+
 (defn base-view [children]
   (html5
    [:head
